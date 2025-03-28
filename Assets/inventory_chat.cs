69d8f0f1b3db4ea3d660e7715ed082052242bb1c
mungy1 <mungydang@gmail.com>
2025-03-29 01:16:08 +0900

    inventory

--- Assets/inventory_chat.cs
+++ Assets/inventory_chat.cs
@@ -1,6 +1,40 @@
 using UnityEngine;
 
-public class inventory_chat : MonoBehaviour
+
+public class Inventory : MonoBehaviour
 {
+    public GameObject Inventory_UI;
+    int setting = 0;
     
+    void Start()
+    {
+        Inventory_UI.SetActive(false);
+    }
+    void Open()
+    {
+        Debug.Log("open");
+        Inventory_UI.SetActive(true);
+    }
+
+    void Close()
+    {
+        Debug.Log("close");
+        Inventory_UI.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            if (setting==0)
+            {
+                Close();
+                setting = 1;
+            }
+
+            else
+            {
+                Open();
+                setting = 0;
+            }
+
+    }
 }
