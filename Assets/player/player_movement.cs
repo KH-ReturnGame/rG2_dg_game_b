8ad47a4e74c5fd57c6670195933f44f4ad8a3768
mungy1 <mungydang@gmail.com>
2025-04-11 07:55:02 +0900

    애니매이터

--- Assets/player/player_movement.cs
+++ Assets/player/player_movement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class player_movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
+    public Animator animator;
     // Update is called once per frame
     void Update()
     {
@@ -15,8 +15,21 @@ public class player_movement : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) moveX = 1f;
         if (Input.GetKey(KeyCode.W)) moveY = 1f;
         if (Input.GetKey(KeyCode.S)) moveY = -1f;
+        
 
         Vector3 moveDirection = new Vector3(moveX, moveY, 0f).normalized;
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+         if (moveDirection.magnitude > 0)
+        {
+            if (!animator.enabled)
+                animator.enabled = true; // 움직이면 Animator 켜기
+        }
+        else
+        {
+            if (animator.enabled)
+                animator.enabled = false; // 멈추면 Animator 끄기
+        }
+    
     }
 }
