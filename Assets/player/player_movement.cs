54ad3a8912ab6ed24aa473f9bd1142fd7b43bfc9
mungy1 <mungydang@gmail.com>
2025-04-11 14:43:16 +0900

    stop

--- Assets/player/player_movement.cs
+++ Assets/player/player_movement.cs
@@ -20,16 +20,7 @@ public class player_movement : MonoBehaviour
         Vector3 moveDirection = new Vector3(moveX, moveY, 0f).normalized;
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
-         if (moveDirection.magnitude > 0)
-        {
-            if (!animator.enabled)
-                animator.enabled = true; // 움직이면 Animator 켜기
-        }
-        else
-        {
-            if (animator.enabled)
-                animator.enabled = false; // 멈추면 Animator 끄기
-        }
+        animator.SetFloat("Speed", moveDirection.magnitude);
     
     }
 }
