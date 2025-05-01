a63eacc96ecf9cde07a914d57839957d1140a8e2
mungy1 <mungydang@gmail.com>
2025-05-01 19:36:15 +0900

    이동 축

--- Assets/player/player_movement.cs
+++ Assets/player/player_movement.cs
@@ -4,7 +4,13 @@ public class player_movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Animator animator;
+    private SpriteRenderer spriteRenderer;
     // Update is called once per frame
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
     void Update()
     {
         float moveX = 0f;
@@ -21,6 +27,11 @@ public class player_movement : MonoBehaviour
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
         animator.SetFloat("Speed", moveDirection.magnitude);
+
+        if (moveX != 0)
+            {
+                spriteRenderer.flipX = moveX < 0; // 왼쪽이면 뒤집기
+            }
     
     }
 }
